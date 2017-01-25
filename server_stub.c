0c56b112d9581df079c9903325521787155af67b
storm2013ak <storm2013.ak@gmail.com>
2017-01-25 11:50:05 -0600

    Update server_stub.c

--- server_stub.c
+++ server_stub.c
@@ -1,3 +1,6 @@
+// to listen on the network 5432
+
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,14 +16,52 @@
 #define MAX_LINE 256
 
 int main() {
-
-  struct sockaddr_in sin;
-  char buf[MAX_LINE];
-  int len;
-  socklen_t new_len;
-  int s, new_s;
   
+  
+    struct sockaddr_in sin; // sin for socket in type of struct.
+    char buf[MAX_LINE];
+    int len; // length
+    socklen_t new_len; // data structure: socklen_t
+    int s, new_s; // s for socket, new_s for new socket. 
 
-  return 0;
+    /* need to build an address data structure */
+    // need to keep up with the state information of connection 
+    bzero((char *) &sin, sizeof(sin)); // bzero expects a pointer
+    sin.sin_family = AF_INET; // address family internet IPv4
+    sin.sin_addr.s_addr = INADDR_ANY; // listen on any interface
+    sin.sin_port = htons(SERVER_PORT); // convert from host to network
+    
+    /* setup passive open */
+    if (( s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket call failed");
+        exit(1);
+    }
+    
+    if ((bind(s, (struct sockaddr *), &sin, sizeof(sin))) < 0)
+    {
+        perror("Bind call failed");
+        exit(1);
+    }
+    
+    listen(s, MAX_PENDING); // listen mode
+
+    /* wait for connection, then receive and print data */
+    while(1)
+    {
+        if (( new_s = accept(s, (struct sockaddr *) &sin, &new_len)) < 0)
+        {
+            perror("accept call failed");
+            exit(1);
+        } 
+        bzero(buf, sizeof(buf));
+        while ((len = recv(new_s, buf, sizeof(buf), 0)) > 0)
+        {
+            fputs(buf, stdout);
+            close(new_s);
+        }
+    }
+
+    return 0;
 }
 

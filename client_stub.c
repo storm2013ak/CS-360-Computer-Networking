d3265f850c87d929be029b0bf4eb431a02fdf45a
storm2013ak <storm2013.ak@gmail.com>
2017-01-25 11:49:35 -0600

    Update client_stub.c

--- client_stub.c
+++ client_stub.c
@@ -13,23 +13,34 @@
 
 int main(int argc, char * argv[]) {
 
-  struct hostent *hp;
-  struct sockaddr_in sin;
-  char *host;
-  char buf[MAX_LINE];
-  int s;
-  int len;
-  
-  if (argc==2) {
-    host = argv[1];
-  }
-  else
-  {
-    fprintf(stderr, "usage: simplex-talk host\n");
-    exit(1);
-  }
-  
+    struct hostent *hp; // host entry
+    struct sockaddr_in sin;
+    char *host; // pointer to host 
+    char buf[MAX_LINE];
+    int s;
+    int len;
+    
+    if (argc==2) {
+        host = argv[1];
+    }
+    else
+    {
+        fprintf(stderr, "usage: simplex-talk host\n");
+        exit(1);
+    }
+    
+    /* translate name to ip addr (invoke resolver) */
+    hp = gethostbyname(host);
+    
+    /* build address data structure */
+
+    /* active open */
+
+    /* connect to the host */
+
+    /* get text from user and sent to server */    
 
   return 0;
 }
+
 

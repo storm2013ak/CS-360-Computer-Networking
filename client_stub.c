01eaac4df4f3c89954b772de8a8665e0afafeccd
storm2013ak <storm2013.ak@gmail.com>
2017-01-27 11:52:22 -0600

    Update client_stub.c

--- client_stub.c
+++ client_stub.c
@@ -1,3 +1,8 @@
+/* Client code:
+    active open
+    connect(send, recv, close)     
+*/
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -30,17 +35,42 @@ int main(int argc, char * argv[]) {
     }
     
     /* translate name to ip addr (invoke resolver) */
-    hp = gethostbyname(host);
+    hp = gethostbyname(host); // gethostbyname only works for IPv4
+    if (!hp) {
+        fprintf(stderr, "simplex-talk: uknown host: %s\n", host);
+        exit(1);
+    }
     
     /* build address data structure */
-
+    bzero((char *)&sin, sizeof(sin));
+    sin.sin_family = AF_INET; // address family internet
+    bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length); // bcopy - byte copy.
+    sin.sin_port = htons(SERVER_PORT); // port is determined by the service 
     /* active open */
-
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket call failed");
+        exit(1);
+    }
     /* connect to the host */
-
+    if (connect(s, (struct sockaddr *) &sin, sizeof(sin)) < 0)
+    {
+        perror("connect call failed");
+        close(s);
+        exit(1);
+    }
+    
     /* get text from user and sent to server */    
+    /* main loop get and send lines of text */
+    while (fgets(buf, sizeof(buf), stdin)) //fgets returns number of bytes that it read
+    {
+        buf[MAX_LINE - 1] = '\0';
+        len = strlen(buf);
+        send(s, buf, len, 0);
+    }
+    
+  
 
   return 0;
 }
-
 
